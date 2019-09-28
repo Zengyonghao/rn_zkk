398c40319798c0dff0039ac96b32d8a936dab5c9
coderzeng <zengyonghaoxxyy@gmail.com>
2019-09-28 15:58:00 +0800

    init

---
+++ src/Home.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './assets/css/login.css'
+function Home() {
+    return(
+        <div className={"parent"}>
+      <div className={"main_content"}>
+
+      </div>
+        <div className={'login_content'} >
+            <div>
+            <nav width="100%" height="300px">
+                <text>销售端</text>
+                <div  className={"line"} />
+                <text>管理端</text>
+            </nav >
+            </div>
+        </div>
+        </div>
+    );
+
+}
+export default Home;
